87066266b9c507c681e456f777e655ff08116415
Subhasish <subhasish469@gmail.com>
2020-09-25 13:06:27 +0530

    first java script

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("hi sabby");
+var fullName="subhasish chatterjee";
+var paymentMethod;
+var courseName="MIT APP INVENTOR";
+const uid="abc123";
+fullName=prompt("Enter your name");
+console.log("full name is : "+fullName);
+var isloggedInFromGoogle=false;
+//uid = "abcd123";
+
+//interpolation
+console.log(`
+    user with name : ${fullName}
+    whose unique id is followed by : ${uid}
+    user with course : ${courseName}
+    who is logged in from google = ${isloggedInFromGoogle}
+
+`);
+
+
